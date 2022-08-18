7291560f412c4c774fd191f97ab7e34054550e70
thefidanabdulla <thefidanabdulla@gmail.com>
2022-08-19 00:26:33 +0400

    Navbar is done

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Container } from 'react-bootstrap';
 import Home from './pages/Home';
 import About from './pages/About';
 import Store from './pages/Store';
+import Navbar from './components/Navbar';
 
 function App() {
   return(
-    <Container className='mb-4'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/store' element={<Store />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/store' element={<Store />} />
+        </Routes>
+      </Container>
+    </>
   )
 }
 
